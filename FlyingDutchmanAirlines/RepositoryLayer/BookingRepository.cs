3195ea3dd079b02866b7e41cd87c6b0645c60b88
Femi Alogba <femialogba@ymail.com>
2023-05-20 10:31:03 +0100

    Add booking repository.

---
+++ FlyingDutchmanAirlines/RepositoryLayer/BookingRepository.cs
@@ -0,0 +1,42 @@
+﻿using FlyingDutchmanAirlines.Data;
+using FlyingDutchmanAirlines.Exceptions;
+using FlyingDutchmanAirlines.Models;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class BookingRepository
+    {
+        private readonly FlyingDutchmanAirlinesContext _context;
+        public BookingRepository(FlyingDutchmanAirlinesContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateBooking(int customerID, int flightID)
+        {
+            if (customerID < 0 || flightID < 0)
+            {
+                throw new ArgumentException("Invalid arguments provided");
+            }
+            Booking booking = new Booking
+            {
+                CustomerId = customerID,
+                FlightNumber = flightID
+            };
+            try
+            {
+                _context.Bookings.Add(booking);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Exception during database query: {exception.Message}");
+                throw new CouldNotAddBookingToDatabaseException();
+            }
+
+        }
+        public async Task<Booking> GetBooking()
+        {
+            return new Booking();
+        }
+    }
+}
