3c36c4bb04d785ea3d1ebb6192c2beaf3482bffc
Femi Alogba <femialogba@ymail.com>
2023-05-20 14:58:53 +0100

    Add overidden save changes method to context stub. This method is modified to check the dbset for pending changes to ensure the database is working as expected.

--- FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
+++ FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlyingDutchmanAirlines.Data;
+using FlyingDutchmanAirlines.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace FlyingDutchmanAirlines_Tests.Stubs
 {
-    public class FlyingDutchmanAirlinesContext_Stub
+    public class FlyingDutchmanAirlinesContext_Stub : FlyingDutchmanAirlinesContext
     {
+        public FlyingDutchmanAirlinesContext_Stub(DbContextOptions<FlyingDutchmanAirlinesContext> options) : base(options)
+        {
+            base.Database.EnsureDeleted();
+        }
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            IEnumerable<EntityEntry> pendingChanges = ChangeTracker.Entries().Where(e => e.State == EntityState.Added);
+            IEnumerable<Booking> bookings = pendingChanges.Select(e => e.Entity).OfType<Booking>();
+
+            if (bookings.Any(b => b.CustomerId != 1))
+            {
+                throw new Exception("Database Error!");
+            }
+            await base.SaveChangesAsync(cancellationToken);
+            return 1;
+        }
     }
 }
