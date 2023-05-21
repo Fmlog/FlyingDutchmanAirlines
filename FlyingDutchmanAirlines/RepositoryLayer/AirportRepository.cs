37760e6e11068838b3546d9717207ffc86cb0ba0
Femi Alogba <femialogba@ymail.com>
2023-05-21 10:18:30 +0100

    Add airport repository.

---
+++ FlyingDutchmanAirlines/RepositoryLayer/AirportRepository.cs
@@ -0,0 +1,28 @@
+﻿using FlyingDutchmanAirlines.Data;
+using FlyingDutchmanAirlines.Exceptions;
+using FlyingDutchmanAirlines.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class AirportRepository
+    {
+        public readonly FlyingDutchmanAirlinesContext _context;
+        public AirportRepository(FlyingDutchmanAirlinesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Airport> GetAirportByID(int airportID)
+        {
+            if (!airportID.IsPositive())
+            {
+                Console.WriteLine($"Argument Exception in GetAirportByID! AirportID = {airportID}");
+                throw new ArgumentException("Invalid Airport Id");
+            }
+
+            return await _context.Airports.FirstOrDefaultAsync(a => a.AirportId == airportID) ?? throw new AirportNotFoundException();
+
+        }
+    }
+}
