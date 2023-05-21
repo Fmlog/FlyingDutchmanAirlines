f9adf8f3142644785c2cc2e3e8d1ce5eba3a0758
Femi Alogba <femialogba@ymail.com>
2023-05-21 11:17:20 +0100

    Add service folder to tests

---
+++ FlyingDutchmanAirlines/ServiceLayer/BookingService.cs
@@ -0,0 +1,31 @@
+﻿using FlyingDutchmanAirlines.Models;
+using FlyingDutchmanAirlines.RepositoryLayer;
+
+namespace FlyingDutchmanAirlines.ServiceLayer
+{
+    public class BookingService
+    {
+        FlightRepository _flightRepository;
+        CustomerRepository _customerRepository;
+        public BookingService(FlightRepository flightRepository,  CustomerRepository customerRepository)
+        {
+            _flightRepository = flightRepository;
+            _customerRepository = customerRepository;
+        }
+        public async Task<bool> CreateBooking(int flightNumber, string customerName)
+        {
+            if (await _flightRepository.GetFlightByID(flightNumber) == null)
+            {
+                throw new Exception();
+            }
+
+                if (await _customerRepository.GetCustomerByName(customerName) == null)
+            {
+                await _customerRepository.CreateCustomer(customerName);
+            }
+            Customer customer = await _customerRepository.GetCustomerByName(customerName);
+
+            return true;
+        }
+    }
+}
