38e3bcbcd1a544b40523e3d2327dec6d2bfbad09
Femi Alogba <femialogba@ymail.com>
2023-05-22 07:46:37 +0100

    Add flight controller tests

---
+++ FlyingDutchmanAirlines_Tests/ControllerLayer/FlightControllerTest.cs
@@ -0,0 +1,13 @@
+﻿namespace FlyingDutchmanAirlines_Tests.ControllerLayer
+{
+    [TestClass]
+    public class FlightControllerTest
+    {
+
+        [TestMethod]
+        public async Task GetFlights()
+        {
+
+        }
+    }
+}
